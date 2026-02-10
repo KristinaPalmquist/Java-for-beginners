a31fd4449bab93358d4161b2c74e8a374f4f1ad7
Kristina <kristina.palmquist@gmail.com>
2026-02-10 15:06:32 +0100

    First classes and functions

---
+++ java-for-beginners/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        HelloWorld myhello = new HelloWorld();
+        String greeting = myhello.sayHello("Kristina");
+        System.out.println(greeting);
+    }
+}
---
+++ java-for-beginners/src/HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public String sayHello(String name) {
+        String greeting = "Hello " + name;
+        return greeting;
+    }
+}
